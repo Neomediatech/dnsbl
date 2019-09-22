9a316ee22174bcadfd75882ab8cd6216289bc357
Dario B <feddar@users.noreply.github.com>
2019-09-22 17:58:54 +0200

    Update 001-create.sql

--- sql/001-create.sql
+++ sql/001-create.sql
@@ -1,35 +1,35 @@
-CREATE DATABASE IF NOT EXISTS `dnsbl` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `dnsbl` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `dnsbl`;
 
 DROP TABLE IF EXISTS `blacklist`;
 CREATE TABLE IF NOT EXISTS `blacklist` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL DEFAULT current_timestamp(),
-  `mail_from` varchar(255) NOT NULL,
-  `mail_to` varchar(255) NOT NULL,
-  `mail_subject` varchar(255) NOT NULL,
-  `env_from` varchar(255) NOT NULL,
-  `from_ip` varchar(15) NOT NULL,
-  `message_id` varchar(255) NOT NULL,
-  `rev_ip` varchar(255) NOT NULL,
-  `helo` varchar(255) NOT NULL,
-  `spam_flag` varchar(255) NOT NULL,
-  `spam_point` decimal(10,0) NOT NULL,
-  `spam_report` text NOT NULL,
-  `alert_service` varchar(255) NOT NULL,
-  `alert_server` varchar(255) NOT NULL,
-  `mail_body` text NOT NULL,
-  `headers` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mail_from` varchar(255) DEFAULT NULL,
+  `mail_to` varchar(255)  DEFAULT NULL,
+  `mail_subject` varchar(255)  NULL,
+  `env_from` varchar(255)  NULL,
+  `from_ip` varchar(15)  NOT NULL,
+  `message_id` varchar(255)  NULL,
+  `rev_ip` varchar(255)  NULL,
+  `helo` varchar(255)  NULL,
+  `spam_flag` varchar(255)  NULL,
+  `spam_point` decimal(10,0) NULL,
+  `spam_report` text  NULL,
+  `alert_service` varchar(255)  NULL,
+  `alert_server` varchar(255)  NULL,
+  `mail_body` text  NULL,
+  `headers` text  NULL,
+  `comment` text  NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `whitelist`;
 CREATE TABLE IF NOT EXISTS `whitelist` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip` varchar(15) DEFAULT NULL,
+  `ip` varchar(15)  DEFAULT NULL,
   `date` datetime DEFAULT current_timestamp(),
   `epoc` timestamp NULL DEFAULT current_timestamp(),
-  `comment` varchar(65000) DEFAULT NULL,
+  `comment` text(65000)  DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
