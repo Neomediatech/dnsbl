8f8022f090964d4ecde3bdcfae879a87ec71fe5f
Dario B <feddar@users.noreply.github.com>
2019-10-15 17:16:57 +0200

    Update 003-pdnsmanager-schema.sql

--- sql/003-pdnsmanager-schema.sql
+++ sql/003-pdnsmanager-schema.sql
@@ -1,1 +1,74 @@
-CREATE DATABASE IF NOT EXISTS `pdnsmanager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `domain_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`domain_id`, `user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `remote`
+--
+
+CREATE TABLE IF NOT EXISTS `remote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `record` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `security` varchar(2000) NOT NULL,
+  `nonce` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `backend` varchar(50) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE IF NOT EXISTS `options` (
+  `name` varchar(255) NOT NULL,
+  `value` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT IGNORE INTO `options` (`name`, `value`) VALUES
+('schema_version', '5');
+
+--
+-- Constraints for table `permissions`
+--
+ALTER IGNORE TABLE `permissions`
+  ADD CONSTRAINT `permissions_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `permissions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `records`
+--
+ALTER IGNORE TABLE `records`
+  ADD CONSTRAINT `records_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `remote`
+--
+ALTER IGNORE TABLE `remote`
+  ADD CONSTRAINT `remote_ibfk_1` FOREIGN KEY (`record`) REFERENCES `records` (`id`) ON DELETE CASCADE;
+
